0ee640ab011da84b6749f285b75f9ba348e77551
aaansari123 <asadali1029@gmail.com>
2022-09-13 20:17:26 -0400

    added database schema and some routes

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create Student model
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      auto: true,
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        max: 280,
+      },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    username: [{
+      type: String,
+      required: true,
+    }],
+
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Reaction = model('reaction', reactionSchema);
+
+module.exports = Reaction;
---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create Student model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    username: [{
+        
+      type: String,
+      ref: 'Users',
+      required: true,
+    }],
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function(){
+    return `${this.reactions.length}`
+})
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = User;
